bcacff78029c9d7cea6d1c5d4b670b9f3ebfca43
Arman Jon Villalobos <me@armanjon.com>
2017-11-10 11:39:22 +0800

    Use params instead of overriding default_uro and validate_url_path to pass params

--- lib/independent_reserve_client/requests/get_exchange_rate_request.rb
+++ lib/independent_reserve_client/requests/get_exchange_rate_request.rb
@@ -7,24 +7,14 @@ module IndependentReserveClient
     private
 
     def path
-      # url should be /PublicGetMarketSummary?
-      # change it to /PublicGetMarketSummary=
-      # because path is not recognized
-      # replace it back in the default_uri method below
-
-      "/Public/GetMarketSummary=primaryCurrencyCode=:primary_currency" +
-        "&secondaryCurrencyCode=:secondary_currency"
+      "/Public/GetMarketSummary"
     end
 
-    def default_uri
-      uri = URI(host)
-      uri.path = api_client_base_path
-      validate_url_path(uri)
-    end
-
-    def validate_url_path(uri)
-      uri_array = uri.to_s.split("=", 2)
-      uri_array[0] + "?" + uri_array[1]
+    def params
+      { 
+        "primaryCurrencyCode" => primary_currency,
+        "secondaryCurrencyCode" => secondary_currency
+      }
     end
   end
 end
